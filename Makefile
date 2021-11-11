adfac7645e5d2565afd203d2f62ff2bbba0f83fd
Kristian Møller Schmidt <kmollerschmidt@gmail.com>
2021-11-11 14:41:03 +0100

    dockerized the app

---
+++ Makefile
@@ -0,0 +1,29 @@
+## ----------------------------------------------------------------------
+## Makefile for 8-puzzle
+##
+## Used for both development and production. See targets below.
+## ----------------------------------------------------------------------
+
+help:   # Show this help.
+	@sed -ne '/@sed/!s/## //p' $(MAKEFILE_LIST)
+	
+
+# ---------- Development ---------- #
+build:  ## Build or rebuild development docker image
+	docker-compose build
+
+develop:  ## Build or rebuild development docker image
+	docker-compose up --remove-orphans
+
+
+# ---------- Testing ---------- #
+test: ## Run test suite
+	python unit_tests.py
+
+
+# ---------- Production ---------- #
+production_stop: ## Stop production server
+	docker-compose -f docker-compose.prod.yml down --remove-orphans
+
+production_start: ## Start production server as daemon
+	docker-compose -f docker-compose.prod.yml up --build --remove-orphans -d
--- app.py
+++ app.py
@@ -1,10 +1,5 @@
- # Setup instuctions for Flask in Visual Studio Code
-# 1) Open folder with flask project
-# 2) In therminal: "pip install flask" (if necessary)
-# 3) In terminal: "python app.py"
-
 from flask import Flask, render_template, request
-import timeit, json
+import json
 from random import choice as random_choice
 from backend.Puzzle import Puzzle
 from backend.utils import convert_solution_string
@@ -12,52 +7,56 @@ from backend.puzzle_collection import eight_puzzles, fifteen_puzzles
 
 app = Flask('__name__')
 
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    
+
     if request.method == 'GET':
         action = "new_sample"
-        puzzle_dim = 3  
+        puzzle_dim = 3
         search_type = "ast"
         puzzle_type = "sample"
-   
-    else: 
-        data = json.loads(request.form["json_data"])   #return(json.dumps(data))
+
+    else:
+        # return(json.dumps(data))
+        data = json.loads(request.form["json_data"])
         action = data["requested_action"]
         puzzle_dim = data["puzzle_dim"]
         search_type = data["search_type"]
         puzzle_type = data["puzzle_type"]
 
-    if action == 'new_sample' or action == 'show_solved_puzzle': # new sample btn or change puzzle size
+    if action == 'new_sample' or action == 'show_solved_puzzle':  # new sample btn or change puzzle size
         if action == 'new_sample':
             if puzzle_dim == 3:
                 puzzle_collection = eight_puzzles
             else:
-                puzzle_collection = fifteen_puzzles 
+                puzzle_collection = fifteen_puzzles
             puzzle_number = random_choice(range(len(puzzle_collection)))
-            puzzle = Puzzle(puzzle_dim, puzzle_dim, puzzle_collection[puzzle_number])._grid 
+            puzzle = Puzzle(puzzle_dim, puzzle_dim,
+                            puzzle_collection[puzzle_number])._grid
             puzzle_title = "Sample #" + str(puzzle_number+1)
             puzzle_is_solved = False
-        else: 
+        else:
             if puzzle_dim == 3:
-                puzzle = [[0,1,2],[3,4,5],[6,7,8]]
+                puzzle = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
             else:
-                puzzle = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
+                puzzle = [[0, 1, 2, 3], [4, 5, 6, 7],
+                          [8, 9, 10, 11], [12, 13, 14, 15]]
             puzzle_title = "Custom puzzle"
-            puzzle_is_solved =  True
-    
+            puzzle_is_solved = True
+
         if puzzle_dim == 3:
             all_search_types = ["ast", "gbfs", "bfs", "dfs"]
- 
+
         elif puzzle_dim == 4:
-            all_search_types = ['ast','gbfs']
+            all_search_types = ['ast', 'gbfs']
 
         data = {
             "puzzle_title": puzzle_title,
             "puzzle": puzzle,
             "original_puzzle": puzzle,
             "puzzle_dim": puzzle_dim,
-            "search_type" : search_type,
+            "search_type": search_type,
             "puzzle_is_solved": puzzle_is_solved,
             "solution_computed": False,
             "requested_action": action,
@@ -68,13 +67,13 @@ def index():
             "all_search_types": all_search_types,
             "search_names":
                 {"ast": "A*-Search",
-                "gbfs": "Greedy Best-First Search ",
-                "dfs": "Depth-First Search",
-                "bfs": "Breadth-First Search"}
+                 "gbfs": "Greedy Best-First Search ",
+                 "dfs": "Depth-First Search",
+                 "bfs": "Breadth-First Search"}
         }
-        
+
     elif action == 'human_move':
-        puzzle = Puzzle(puzzle_dim, puzzle_dim, data["puzzle"]);    
+        puzzle = Puzzle(puzzle_dim, puzzle_dim, data["puzzle"])
         try:
             puzzle.update_puzzle(data["direction"])
             data["puzzle"] = puzzle._grid
@@ -91,50 +90,53 @@ def index():
             pass   # Move is off grid
 
     elif action == 'solve_puzzle':
-        puzzle = Puzzle(puzzle_dim, puzzle_dim, data["puzzle"]);    
+        puzzle = Puzzle(puzzle_dim, puzzle_dim, data["puzzle"])
         solution_string, num_expanded_nodes, max_search_depth, running_time, max_ram_usage \
-                = puzzle.solve_puzzle(data['search_type'])
+            = puzzle.solve_puzzle(data['search_type'])
         data['running_time'] = round(running_time, 5)
-        data['max_ram_usage'] = round(max_ram_usage, 2)  #number is in megabytes
+        data['max_ram_usage'] = round(
+            max_ram_usage, 2)  # number is in megabytes
         data['num_solution_steps'] = len(solution_string)
         data['num_expanded_nodes'] = num_expanded_nodes
         data['max_search_depth'] = max_search_depth
         data['solution_string'] = solution_string
         data["solution_computed"] = True
-        data["show_solution_details"] =  True
+        data["show_solution_details"] = True
         if data["puzzle_type"] == "sample":
             data["solve_or_reset_btn_value"] = "Reset Sample"
-        else: 
+        else:
             data["solve_or_reset_btn_value"] = "Reset Custom"
-        
+
         # Compute all board positions on the road to solution:
         puzzle_clone = puzzle.clone()
         animated_solution = []
         for direction in solution_string:
             puzzle_clone.update_puzzle(direction)
-            animated_solution.append(puzzle_clone.clone()._grid)    
-        data['animated_solution'] = animated_solution 
+            animated_solution.append(puzzle_clone.clone()._grid)
+        data['animated_solution'] = animated_solution
         data["final_state"] = animated_solution[-1]
 
-        
-    elif action == 'reset': 
+    elif action == 'reset':
         data["solution_computed"] = False
         data["solve_or_reset_btn_value"] = "Solve"
         data["show_solution_details"] = False
         data["puzzle"] = data["original_puzzle"]
         if data["puzzle_type"] == "sample":
-            data["move_count"] = 0         
-    
-    return render_template("index.html", data = data)
+            data["move_count"] = 0
+
+    return render_template("index.html", data=data)
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/moves", methods=['POST'])
 def moves():
-    data = json.loads(request.form["json_data"])   
-    return str(convert_solution_string(data["solution_string"]))  
+    data = json.loads(request.form["json_data"])
+    return str(convert_solution_string(data["solution_string"]))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
